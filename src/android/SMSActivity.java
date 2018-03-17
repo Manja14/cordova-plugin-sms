b7260919007baa1a8dc84490b23e52f94112b80a
Manja Kocet <manja.kocet@gmail.com>
2018-03-17 14:48:06 +0100

    SMS receiver

---
+++ src/android/SMSActivity.java
@@ -0,0 +1,70 @@
+package com.rjfun.cordova.sms;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.util.Map;
+import java.util.HashMap;
+import org.json.JSONObject;
+
+public class SMSluginActivity extends Activity {
+    private static String TAG = "SMSPlugin";
+
+    /*
+     * this activity will be started if the user touches a notification that we own. 
+     * We send it's data off to the push plugin for processing.
+     * If needed, we boot up the main activity to kickstart the application. 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		Log.d(TAG, "==> SMSPluginActivity onCreate");
+		
+		JSONObject json = new JSONObject();
+        if (getIntent().getExtras() != null) {
+            json = getIntent().getExtras();
+			Log.d(TAG, "==> SMS RECEIVED");
+			data.put("navCoords", json);
+        }
+		
+        SMSPlugin.onSMSArrive(json);
+
+        finish();
+
+        forceMainActivityReload();
+    }
+
+    private void forceMainActivityReload() {
+        PackageManager pm = getPackageManager();
+        Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
+        startActivity(launchIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+		Log.d(TAG, "==> SMSPluginActivity onResume");
+    }
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "==> SMSPluginActivity onStart");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG, "==> SMSPluginActivity onStop");
+	}
+
+}
---
+++ src/android/SMSReceiver.java
@@ -0,0 +1,88 @@
+package com.rjfun.cordova.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+public class SmsReceiver extends BroadcastReceiver
+{
+    private static String TAG = "SmsReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Log.i(TAG, "in onReceive");
+
+        Bundle bundle = intent.getExtras();
+        if (bundle != null)
+        {
+            Log.i(TAG, "Reading Bundle");
+
+            SmsMessage smsMessage;
+            if (Build.VERSION.SDK_INT >= 19)
+            { //KITKAT
+                Log.i(TAG, "Version >= 19");
+                SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                smsMessage = msgs[0];
+            } else
+            {
+                Log.i(TAG, "Version < 19");
+                Object pdus[] = (Object[]) bundle.get("pdus");
+                smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
+            }
+
+            JSONObject json = getJsonFromSmsMessage(smsMessage);
+
+            SMSPlugin.onSMSArrive(json);
+
+            if (smsMessage.getMessageBody().toLowerCase().contains("test123"))
+            {
+                startActivity(json);
+            }
+        }
+
+        Log.i(TAG, "out onReceive");
+    }
+
+    /**
+     * Open app when received FCM message.
+     *
+     * @param data FCM data.
+     */
+    private void startActivity(JSONObject data)
+    {
+        Intent intent = new Intent(this, FCMPluginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("navCoords", data);
+        this.startActivity(intent);
+    }
+
+
+    private JSONObject getJsonFromSmsMessage(SmsMessage sms) {
+    	JSONObject json = new JSONObject();
+    	
+        try {
+        	json.put( ADDRESS, sms.getOriginatingAddress() );
+        	json.put( BODY, sms.getMessageBody() ); // May need sms.getMessageBody.toString()
+        	json.put( DATE_SENT, sms.getTimestampMillis() );
+        	json.put( DATE, System.currentTimeMillis() );
+        	json.put( READ, MESSAGE_IS_NOT_READ );
+        	json.put( SEEN, MESSAGE_IS_NOT_SEEN );
+        	json.put( STATUS, sms.getStatus() );
+        	json.put( TYPE, MESSAGE_TYPE_INBOX );
+        	json.put( SERVICE_CENTER, sms.getServiceCenterAddress());
+        	
+        } catch ( Exception e ) { 
+            e.printStackTrace(); 
+        }
+
+    	return json;
+    }
+}
